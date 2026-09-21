50eb0ced8967708c1ebb4a7848005e1efc0db248
Julian Passebecq <64646018+julian-passebecq@users.noreply.github.com>
2026-09-21 18:13:56 +0200

    feat: wire Headlamp deep link from runtime config

--- src/pages/KubernetesPage.tsx
+++ src/pages/KubernetesPage.tsx
@@ -1,8 +1,10 @@
 import { Button, Card, Spinner, Text, Title3 } from "@fluentui/react-components";
 import { useOverview, useWorkloads } from "../api/queries";
+import { ExternalLinkButton } from "../components/ExternalLinkButton";
 import { MetricCard } from "../components/MetricCard";
 import { PageHeader } from "../components/PageHeader";
 import { WorkloadTable } from "../components/WorkloadTable";
+import { runtimeConfig } from "../config";
 
 export function KubernetesPage() {
   const overview = useOverview();
@@ -12,7 +14,7 @@ export function KubernetesPage() {
   const podTotal = namespaces.reduce((sum, item) => sum + item.podsTotal, 0);
   const podReady = namespaces.reduce((sum, item) => sum + item.podsReady, 0);
   return <>
-    <PageHeader title="Kubernetes" subtitle="Single-node K3s operations" actions={<Button>Open Headlamp</Button>} />
+    <PageHeader title="Kubernetes" subtitle="Single-node K3s operations" actions={<ExternalLinkButton href={runtimeConfig.headlampUrl}>Open Headlamp</ExternalLinkButton>} />
     <section className="metrics">
       <MetricCard label="Node" value="Ready" detail={overview.data.vm.name} />
       <MetricCard label="Pods" value={podReady + " / " + podTotal} detail="Ready / total" />
